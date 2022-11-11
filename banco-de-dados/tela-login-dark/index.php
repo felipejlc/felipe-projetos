815bf5aed90710fc44e9c6828c93f6ed95bda5fe
felipejlc <113310248+felipejlc@users.noreply.github.com>
2022-11-11 13:04:37 -0300

    atualização bd

--- banco-de-dados/tela-login-dark/index.php
+++ banco-de-dados/tela-login-dark/index.php
@@ -1,3 +1,20 @@
+<?php 
+
+    if(isset($_POST['submit']))
+    {
+        // print_r($_POST['nome']);
+        // print_r('<br>');
+        // print_r($_POST['senha']);
+
+        include_once('config.php');
+
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
+
+        $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,senha) VALUES ('$nome','$senha')");
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -13,10 +30,10 @@
             <div>
                 <img src="imagens/logo.png" alt="Logo">
             </div>
-            <form method="post">
+            <form action="index.php" method="post">
                 <input type="text" name="nome" placeholder="nome de usuário" autofocus>
                 <input type="password" name="senha" placeholder="sua senha">
-                <input type="submit" value="entrar">
+                <input type="submit" name="submit" value="entrar">
             </form>
             <p>Ainda não tem uma conta?<a href="#">Criar conta</a></p>
         </div>
