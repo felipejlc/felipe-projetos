ce5df3bdba3d8d7e4b1b151e23ef9d9f08cdd7b6
Felipe Costa <113310248+felipejlc@users.noreply.github.com>
2023-01-05 08:34:37 -0300

    projeto carros criado

---
+++ projeto-carros/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+	<head>
+		<title>Itaipú Veículos</title>
+		<link rel="stylesheet" href="estilos.css"/>
+	</head>
+	<body>
+	
+		<header>
+			<?php
+				include "topo.php";
+			?>
+		</header>
+	
+	</body>
+</html>
